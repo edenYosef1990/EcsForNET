74c4897209a0f25e67c49c03fa30ab8a193ce4df
Eden Yosef <bla@a.com>
2023-08-03 14:10:31 +0300

    Add project files.

---
+++ ConsoleApp4/EcsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public abstract class ComponentBase
+    {
+        public int id;
+    }
+    public class EcsRepository
+    {
+        public T GetComponentForId<T>(int entityId, Guid ComponentTypeGuid) where T: ComponentBase {
+           if(!ComponentsRepositroy.TryGetValue(ComponentTypeGuid, out var ComponentRepository))
+            {
+                throw new Exception("Invalid Type GUID!");
+            }
+            return (T)ComponentRepository.First(x => x.id == entityId);
+        }
+
+        public void SetComponentForId<T>(T component) where T: ComponentBase, new()
+        {
+            var ComponentTypeGuid = typeof(T).GUID;
+            if (!ComponentsRepositroy.TryGetValue(ComponentTypeGuid, out var ComponentRepository))
+            {
+                ComponentsRepositroy.Add(ComponentTypeGuid,new List<ComponentBase>());
+            }
+            ComponentRepository!.Add(component);
+        }
+
+        public void SetRelation<R>(int leftEntity, int RightEntity)
+        {
+            var relationTypeGuid = typeof(R).GUID;
+            if (!RelationsRepositroy.TryGetValue(relationTypeGuid, out var relationRepository))
+            {
+                RelationsRepositroy.Add(relationTypeGuid, new List<(int,int)>());
+            }
+            relationRepository!.Add((leftEntity, RightEntity));
+        }
+
+        public Dictionary<Guid, List<(int,int)>> RelationsRepositroy;
+        public Dictionary<Guid, List<ComponentBase>> ComponentsRepositroy;
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp4
+{
+    public interface IQuery { }
+
+    public class FunctionContainer
+    {
+        public FunctionContainer(Action<IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery, IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
+        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
+    }
+
+    public class EdenGenericClass<T,K>
+    {
+        public T val { get; set; }
+
+        public EdenGenericClass(T t)
+        {
+            val = t;
+        }
+    }
+
+    internal class Program
+    {
+        public void foo(int arg1, int arg2, string arg3, EdenGenericClass<int,string> arg4)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            var bla = typeof(Program)!.GetMethod("foo")!.GetParameters();
+            var myType = bla[3].ParameterType;
+            foreach (var para in bla) {
+                Console.WriteLine(para.ParameterType.ToString());
+                if (para.ParameterType.IsConstructedGenericType) {
+                    Console.WriteLine("generic!");
+                    foreach(var genericType in para.ParameterType.GetGenericArguments()) {
+                        Console.WriteLine("\t"+genericType.ToString());
+                    }
+                }
+            }    
+        }
+    }
+}
