b1d7f411209e7c58d73c5dbb69eee3607bea841f
Eden Yosef <bla@a.com>
2023-08-06 09:22:14 +0300

    add Archtype class and hash function

---
+++ ConsoleApp4/Archtype.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp4;
+
+public class Archtype
+    {
+        private Guid XorGuids(Guid guid1, Guid guid2)
+        {
+            const int BYTECOUNT = 16;
+            byte[] destByte = new byte[BYTECOUNT];
+            byte[] guid1Byte = guid1.ToByteArray();
+            byte[] guid2Byte = guid2.ToByteArray();
+
+            for (int i = 0; i < BYTECOUNT; i++)
+            {
+                destByte[i] = (byte)(guid1Byte[i] ^ guid2Byte[i]);
+            }
+            return new Guid(destByte);
+        }
+
+        public Guid HashingGuidsVector(Guid[] guids)
+        {
+            var result = Guid.Empty;
+            foreach (var guid in guids)
+            {
+                result = XorGuids(result,guid);
+            }
+            return result;
+        }
+    }
