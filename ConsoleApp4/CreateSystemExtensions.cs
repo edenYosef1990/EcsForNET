e41c6aff44bdf45e0c5b5eee1d8b1d0d2d3cc2e9
Eden Yosef <bla@a.com>
2023-08-07 14:54:15 +0300

    refactored generic code

---
+++ ConsoleApp4/CreateSystemExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp4;
+
+public class QueryClass<T> : IQuery
+{
+    public QueryClass(T val) { }
+}
+
+public interface IQuery { }
+public static class CreateSystemExtensions
+{
+    public static void CreateSystem<A>(Action<A> systemFunction)
+    where A : IQuery
+    {
+        var bla = systemFunction.GetMethodInfo().GetParameters();
+        foreach (var para in bla)
+        {
+            Console.WriteLine(para.ParameterType.ToString());
+            var stam = para.GetType();
+            if (para.ParameterType.IsConstructedGenericType)
+            {
+                Console.WriteLine("generic!");
+                foreach (var genericType in para.ParameterType.GetGenericArguments())
+                {
+                    Console.WriteLine("\t" + genericType.ToString());
+                }
+            }
+        }
+    }
+
+    public static void CreateSystem<A, B>(Action<A, B> systemFunction)
+        where A : IQuery
+        where B : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C>(Action<A, B, C> systemFunction)
+        where A : IQuery
+        where B : IQuery
+        where C : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C, D>(Action<A, B, C, D> systemFunction)
+        where A : IQuery
+        where B : IQuery
+        where C : IQuery
+        where D : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C, D, E>(Action<A, B, C, D, E> systemFunction)
+    where A : IQuery
+    where B : IQuery
+    where C : IQuery
+    where D : IQuery
+    where E : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C, D, E, F>(Action<A, B, C, D, E, F> systemFunction)
+    where A : IQuery
+    where B : IQuery
+    where C : IQuery
+    where D : IQuery
+    where E : IQuery
+    where F : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C, D, E, F, G, H>(Action<A, B, C, D, E, F, G> systemFunction)
+    where A : IQuery
+    where B : IQuery
+    where C : IQuery
+    where D : IQuery
+    where E : IQuery
+    where F : IQuery
+    where G : IQuery
+    { }
+
+    public static void CreateSystem<A, B, C, D, E, F, G ,H>(Action<A, B, C, D, E, F, G, H> systemFunction)
+    where A : IQuery
+    where B : IQuery
+    where C : IQuery
+    where D : IQuery
+    where E : IQuery
+    where F : IQuery
+    where G : IQuery
+    where H : IQuery
+    { }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,19 +1,13 @@
 ﻿namespace ConsoleApp4
 {
-    public interface IQuery { }
 
-    public class FunctionContainer
+    public class Bla<T> : IQuery
     {
-        public FunctionContainer(Action<IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery, IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
-        public FunctionContainer(Action<IQuery, IQuery, IQuery, IQuery, IQuery, IQuery, IQuery> systemFunction) { }
+        public Bla(T t) { }
     }
+    public interface GenericInteface { }
 
-    public class EdenGenericClass<T, K>
+    public class EdenGenericClass<T, K> : GenericInteface
     {
         public T val { get; set; }
 
@@ -25,32 +19,23 @@
 
     internal class Program
     {
-        public void foo(int arg1, int arg2, string arg3, EdenGenericClass<int, string> arg4)
+        public void foo(int arg1, int arg2, string arg3, GenericInteface arg4)
         {
 
         }
 
         static void Main(string[] args)
         {
-            //var bla = typeof(Program)!.GetMethod("foo")!.GetParameters();
-            //var myType = bla[3].ParameterType;
-            //foreach (var para in bla) {
-            //    Console.WriteLine(para.ParameterType.ToString());
-            //    if (para.ParameterType.IsConstructedGenericType) {
-            //        Console.WriteLine("generic!");
-            //        foreach(var genericType in para.ParameterType.GetGenericArguments()) {
-            //            Console.WriteLine("\t"+genericType.ToString());
-            //        }
-            //    }
-            //}    
-            var guid1 = Guid.NewGuid();
-            var guid2 = Guid.NewGuid();
-            var guid3 = Guid.NewGuid();
-            var guids = new Guid[] { guid1, guid2, guid3 };
-            var cache = new ArchTypeHashingCache();
-            var res = cache.GetHashFromSortedVector(guids);
-            var otherRes = new Guid[] { guid1, guid2, guid3 }.HashingGuidsVector(0, 2);
-            var isEqual = res.Equals(otherRes);
+            CreateSystemExtensions.CreateSystem(((Bla<int> a) => { }));
+            
+            //var guid1 = Guid.NewGuid();
+            //var guid2 = Guid.NewGuid();
+            //var guid3 = Guid.NewGuid();
+            //var guids = new Guid[] { guid1, guid2, guid3 };
+            //var cache = new ArchTypeHashingCache();
+            //var res = cache.GetHashFromSortedVector(guids);
+            //var otherRes = new Guid[] { guid1, guid2, guid3 }.HashingGuidsVector(0, 2);
+            //var isEqual = res.Equals(otherRes);
         }
     }
 }
